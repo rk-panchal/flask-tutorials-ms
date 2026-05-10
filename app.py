aba4e516475053a15e36121be88041dac69cd3b6
rk-panchal <panchalrk89@gmail.com>
2026-05-10 17:52:41 +0530

    Save data in Mongo Atlas Cluster

--- app.py
+++ app.py
@@ -1,15 +1,37 @@
 #from flask import Flask # need to import Flask class from the flask module to create a Flask application instance
 # from flask import Flask, request # we also import 'request' to handle incoming request data in our API endpoints
 from flask import Flask, request, render_template # we also import 'render_template' to render HTML templates
-from datetime import datetime # we import 'datetime' to work with date and time in our API endpoints
+from datetime import datetime
+import pymongo # we import 'datetime' to work with date and time in our API endpoints
+from dotenv import load_dotenv # we import 'load_dotenv' to load environment variables from a .env file
+from urllib.parse import quote_plus
+import os # we import 'os' to access environment variables
+# from pymongo import MongoClient # we import 'MongoClient' to connect to our MongoDB
+import pymongo # we import 'pymongo' to connect to our MongoDB database and perform database operations
+from pymongo import MongoClient
+from bson import ObjectId
+
+load_dotenv() # we call 'load_dotenv()' to load environment variables from the .env file
+
+username = quote_plus(os.getenv("MONGO_USER"))
+password = quote_plus(os.getenv("MONGO_PASS"))
+
+MONGO_URI = f"mongodb+srv://{username}:{password}@rk.9hyvp36.mongodb.net/?appName=RK"
+
+client = MongoClient(MONGO_URI)
+
+db = client["test"]   # database
+collection = db["flask_tutorial"]  # collection
 
 app = Flask(__name__)
+
 @app.route('/')
 def home():
     # return 'Hello, Flask! Welcome to My Home page'
     day_of_week = datetime.now().strftime('%A') # we get the current day of the week using 'datetime.now()' and format it as a string using 'strftime()'
     current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S') # we get the current date and time and format it as a string
     return render_template('index.html', day_of_week=day_of_week, current_time=current_time) # we render the 'index.html' template and pass the 'day_of
+
 @app.route('/about')
 def about():
     return 'This is the about page of my Flask app'
@@ -85,9 +107,33 @@ def show_request_data():
         'age': age
     }
     return response
+
 @app.route('/time')
 def time():
     return {'current_time': datetime.now().strftime('%Y-%m-%d %H:%M:%S')}
-     # we specify that this endpoint only accepts POST requests
+
+@app.route('/submit', methods=['POST']) # we specify that this endpoint only accepts POST requests
+def submit():
+    # form_data = request.form.to_dict()
+    form_data = dict(request.form) # we can get all form data as a dictionary using 'request.form.to_dict()''
+    result = collection.insert_one(form_data)
+    form_data["_id"] = str(result.inserted_id)
+    return form_data    
+    # username = request.form.get('username') # we can get form data from the request using 'request.form.get()'
+    # if username:
+    #     return f'Thank you for signing up, {username}!'
+    # else:
+    #     return 'Username is required.', 400
+
+@app.route('/users')
+def get_users():
+    users = list(collection.find()) # we retrieve all documents from the 'users' collection and convert the cursor to a list
+    for user in users:
+        user['_id'] = str(user['_id']) # we convert the ObjectId to a string for JSON serialization
+    print ('Retrieved users from database:', users) # we print the retrieved users to the console for debugging
+    for user in users:
+        del user['_id'] # we remove the '_id' field from each user document before returning the response, since it's not needed in the API response
+    return {'users': users} # we return the list of users as a JSON response
+
 if __name__ == '__main__':
     app.run(debug=True) 
